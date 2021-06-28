249f88baee9cef086e1a4c5dfb1edf72de328c9c
Nurina <nurina.junus@gmail.com>
2021-06-28 13:57:57 -0700

    commit tree a603efb2d9fb21c4442a7610acdfdedcc1d292a2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+const submitData = ( a, b ) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: a,
+            email: b
+        })
+    })
+            .then(response => response.json())
+            .then(object => document.body.append(object.id))
+            .catch(object => document.body.append(object.message))
+}
